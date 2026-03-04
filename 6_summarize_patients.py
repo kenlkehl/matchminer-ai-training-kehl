432af07b8bbf8ef2a11372c0c0716468436ae012
Ken Kehl <kenneth_kehl@dfci.harvard.edu>
2026-03-04 20:56:11 +0000

    fixing summarizer

--- 6_summarize_patients.py
+++ 6_summarize_patients.py
@@ -250,7 +250,7 @@ Histology: (patient's histology (eg adenocarcinoma, squamous carcinoma, etc))
 Current extent: (patient's current extent (localized, advanced, metastatic, etc))
 Tumor markers: (tumor markers for following disease status over time, such as CEA or PSA, go here)
 Biomarkers: (genomic results, protein expression, etc, relevant or potentially relevant for informing treatment selection. Err on the side of including all possible biomarkers, including all IHC results, all positive genomic findings, and any pertinent negative genomic findings)
-Treatment history: (surgery, radiation, chemotherapy/targeted therapy/immunotherapy, etc, including start and stop dates; include any disease response and/or progression/relapse/recurrence events in the course of each  treatment)
+Treatment history: (surgery, radiation, chemotherapy/targeted therapy/immunotherapy, etc, including start and stop dates; include any disease response and/or progression/relapse/recurrence events in the course of each treatment WITHIN this section, not as a separate section)
 Boilerplate: (any history of conditions that might meet common "boilerplate" exclusion criteria for clinical trials, such as uncontrolled brain metastases, lack of measurable disease, congestive heart failure, pneumonitis, renal dysfunction, liver dysfunction, HIV or hepatitis infection, etc)
 Clearly separate the "boilerplate" section by labeling it "Boilerplate: " before describing any such conditions.
 --(end of sections)
