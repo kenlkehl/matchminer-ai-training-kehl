7dd1c42a3800a7f939f7b827173ef61b7e0f5a73
kenlkehl <kenkehl@gmail.com>
2026-03-11 19:46:57 -0400

    trialcheck train script udpate

--- 15_train_modernbert_trial_checker.py
+++ 15_train_modernbert_trial_checker.py
@@ -108,7 +108,7 @@ def main(checkpoint_dir: str, output_dir: str):
         args=training_args,
         train_dataset=tokenized_data["train"],
         #eval_dataset=tokenized_data["valid"],
-        tokenizer=tokenizer,
+        processing_class=tokenizer,
         data_collator=data_collator
         #compute_metrics=compute_metrics,
     )
